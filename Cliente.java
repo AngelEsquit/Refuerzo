e4de0bce29f8028a47a9b279b1680066bf1302fe
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-12 21:33:17 -0600

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,38 @@
 public class Cliente {
-    
+    private int num;
+    private String nombre;
+    private String direccion;
+    private int prestamos;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(int prestamos) {
+        this.prestamos = prestamos;
+    }
 }
