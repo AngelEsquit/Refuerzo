85e28ebe101f887fed6c1dc4ff6bf16dba1d5ad9
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-12 21:37:12 -0600

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,66 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Biblioteca {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int salir = 1;
+        int opcion = 0;
+
+        while (salir != 0) {
+            printMenu();
+            try {
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+            
+            switch (opcion) {
+                case 1:
+                    System.out.println("// Lógica para la opción 1");
+                    break;
+                case 2:
+                    System.out.println("// Lógica para la opción 2");
+                    break;
+                case 3:
+                    System.out.println("// Lógica para la opción 3");
+                    break;
+                case 4:
+                    System.out.println("// Lógica para la opción 4");
+                    break;
+                case 5:
+                    salir = 0;
+                    System.out.println("// Salir del bucle");
+                    break;
+                default:
+                    System.out.println("");
+                    System.out.println("Número inválido. Intente nuevamente.");
+                    break;
+            }
+
+            opcion = 0;
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("*************************************");
+        System.out.println("                Menú");
+        System.out.println("*************************************");
+        System.out.println("Ingrese la opción que desee:");
+        System.out.println("1: Ingreso monetario");
+        System.out.println("2: Balance");
+        System.out.println("3: Consejos extras");
+        System.out.println("4: Consulta de saldo por fechas");
+        System.out.println("5: Salir");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
 }
