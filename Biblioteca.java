7cabcd1bf3265ad5311f22915b45ca411c7a7d29
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-12 23:01:22 -0600

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -8,9 +8,20 @@ public class Biblioteca {
         Scanner scanner = new Scanner(System.in);
         int salir = 1;
         int opcion = 0;
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
+        int num;
+        String titulo;
+        String materia;
+        int cantidad;
+        String estado = "Disponible";
+        String autor;
+        String editorial;
+        int anio;
 
         while (salir != 0) {
             printMenu1();
+
             try {
                 opcion = scanner.nextInt();
                 scanner.nextLine();
@@ -19,23 +30,49 @@ public class Biblioteca {
                 System.out.println("Ingrese un número.");
                 scanner.nextLine();
             }
-            
+
             switch (opcion) {
                 case 1:
-                    System.out.println("// Lógica para la opción 1");
+                    printMenu2();
+
+                    try {
+                        opcion = scanner.nextInt();
+                        scanner.nextLine();
+                    } catch (InputMismatchException e) {
+                        System.out.println("");
+                        System.out.println("Ingrese un número.");
+                        scanner.nextLine();
+                    }
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Ingrese el número del libro:");
+                            num = scanner.nextInt();
+                            scanner.nextLine();
+                    
+                            System.out.println("Ingrese el título del libro:");
+                            titulo = scanner.nextLine();
+                    
+                            System.out.println("Ingrese la materia del libro:");
+                            materia = scanner.nextLine();
+                    
+                            System.out.println("Ingrese la cantidad de libros:");
+                            cantidad = scanner.nextInt();
+                            scanner.nextLine();
+                    
+                            System.out.println("Ingrese el autor del libro:");
+                            autor = scanner.nextLine();
+                    
+                            System.out.println("Ingrese la editorial del libro:");
+                            editorial = scanner.nextLine();
+
+                            items.add(new Libro(num, titulo, materia, cantidad, estado, autor, editorial));
+                    }
+
                     break;
                 case 2:
-                    System.out.println("// Lógica para la opción 2");
                     break;
                 case 3:
-                    System.out.println("// Lógica para la opción 3");
-                    break;
-                case 4:
-                    System.out.println("// Lógica para la opción 4");
-                    break;
-                case 5:
-                    salir = 0;
-                    System.out.println("// Salir del bucle");
                     break;
                 default:
                     System.out.println("");
@@ -44,6 +81,30 @@ public class Biblioteca {
             }
 
             opcion = 0;
+            estado = "Disponible";
+        }
+    }
+
+    public static void verificarCliente (int num, ArrayList<Cliente> clientes) {
+        int control = 0;
+
+        for (Cliente cliente : clientes) {
+            if (cliente.getNum() == num) {
+                control = 1;
+                break;
+            }
+
+            else {
+                continue;
+            }
+        }
+
+        if (control == 0) {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("El número de identificación de esa persona no está registrado.");
+            System.out.println("");
+            System.out.println("");
         }
     }
 
@@ -54,12 +115,25 @@ public class Biblioteca {
         System.out.println("                Menú");
         System.out.println("*************************************");
         System.out.println("Ingrese la opción que desee:");
-        System.out.println("1: Ingreso monetario");
-        System.out.println("2: Balance");
-        System.out.println("3: Consejos extras");
-        System.out.println("4: Consulta de saldo por fechas");
-        System.out.println("5: Salir");
+        System.out.println("1: Ingresar nuevo ítem.");
+        System.out.println("2: Buscar una publicación por # de identificación.");
+        System.out.println("3: Cantidad de ítems de una materia.");
+        System.out.println("4: Cantidad de revistas de una materia.");
+        System.out.println("5: Mostrar información de libros.");
+        System.out.println("6: Registrar préstamo.");
+        System.out.println("7: Salir");
         System.out.println("");
+        System.out.println("");
+    }
+
+    public static void printMenu2() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Ingrese la opción que desee:");
+        System.out.println("1: Libro");
+        System.out.println("2: Revista");
+        System.out.println("3: Artículo");
+        System.out.println("4: Salir");
         System.out.println("");
         System.out.println("");
     }
