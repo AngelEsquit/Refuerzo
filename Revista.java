7a6f34e0b8d1bf5a2202826a725c4a97858297b5
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-12 21:31:13 -0600

    Update Revista.java

--- Revista.java
+++ Revista.java
@@ -1,3 +1,11 @@
-public class Revista {
-    
+public class Revista extends Item {
+    private int anio;
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }    
 }
