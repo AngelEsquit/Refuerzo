3a4666c09faf3093f3788a6fe9e7c943ee9e2afb
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-12 21:26:52 -0600

    1

---
+++ Item.java
@@ -0,0 +1,46 @@
+public class Item {
+    protected int num;
+    protected String titulo;
+    protected String materia;
+    protected int cantidad;
+    protected String estado;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
